948c77498992e1a36dd884e9e4b6594205cae176
silje <angelviksilje@gmail.com>
2024-04-24 12:44:05 +0200

    DAY 7 "JavaScript Introduction" - Added Color Theme to Page

--- js/day-7.js
+++ js/day-7.js
@@ -1,3 +1,4 @@
+// Time: Change Greeting
 let kodeloftet = "Kodeloftet";
 
 let time = new Date().getHours(),
@@ -7,3 +8,24 @@ document.getElementById('greetingOutput').innerHTML = greeting;
 
 console.log(time);
 console.log(greeting);
+
+// Button: Change Theme
+let buttonChangeTheme = document.getElementById("theme-changer");
+
+function changeTheme() {
+  // const body = document.getElementById("body");
+  const root = document.documentElement;
+
+  const currentTheme = root.dataset.theme;
+  // const changeTheme =
+
+  const isLight = currentTheme === "light";
+
+  if (isLight) {
+    root.dataset.theme = "dark";
+  } else {
+    root.dataset.theme = "light";
+  }
+}
+
+buttonChangeTheme.addEventListener('click', changeTheme);
